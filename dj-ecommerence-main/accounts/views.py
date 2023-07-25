cc261fa386449d051ed29ef59a4b4430ba40f8c7
mabass123 <tornedo2100@hotmail.com>
2023-07-25 21:03:45 +0300

    'final'

---
+++ dj-ecommerence-main/accounts/views.py
@@ -0,0 +1,171 @@
+from carts.models import Cart, Cart_Item
+from django import VERSION
+from django.contrib.sites.requests import RequestSite
+from django.http.response import HttpResponse
+from accounts.models import Account
+from django.shortcuts import redirect, render
+from .forms import RegistrationForm
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from store.views import _cart_id
+# Verification
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+import requests
+# Create your views here.
+
+def register (request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Please check your email for verification email, please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+def login (request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = Cart_Item.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = Cart_Item.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+            
+        else:
+            messages.error(request, 'invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def logout (request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect ('home')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+@login_required(login_url='login')
+def dashboard (request):
+    return render(request, 'accounts/dashboard.html')
+
+def forgotpassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            
+            current_site = get_current_site(request)
+            mail_subject = 'Password Reset'
+            message = render_to_string('accounts/forgotpassword_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password Reset Has Been Sent, Check Your email')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgotpassword')
+    return render(request, 'accounts/forgotpassword.html')
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset you password')
+        return redirect('resetpassword')
+    else:
+        messages.error(request, 'This link is expired')
+        return redirect('login')
+
+def resetpassword(request):
+    if request.method == 'POST':
+        password = request.POST['CreatePassword']
+        confirm_password = request.POST['ConfirmPassword']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reseted succesfully')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password Does not Match')
+            return redirect('resetpassword')
+    else:
+        return render(request, 'accounts/resetpassword.html')
---
+++ dj-ecommerence-main/carts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Cart, Cart_Item
+# Register your models here.
+class CartItemAdmin (admin.ModelAdmin):
+    list_display = ['cart_id']
+class CartAdmin (admin.ModelAdmin):
+    list_display = ['product', 'cart', 'quantity', 'is_active']
+    list_editable = ['is_active',]
+admin.site.register(Cart,CartItemAdmin)
+admin.site.register(Cart_Item,CartAdmin)
+
